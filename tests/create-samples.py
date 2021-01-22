c08e61fa402e5e5ca805ae90348a8ca614920eec
Amélia SZK <amelia.szk@protonmail.com>
2021-01-21 19:45:34 -0500

    samp: pythonList presumably well formed

--- tests/create-samples.py
+++ tests/create-samples.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 blue_12     = ( 10,  20, 230, 252)
 green_10    = ( 10, 220,  30, 250)
@@ -24,8 +25,42 @@ cells = [
     [ gre, gre, red, red, red, red, blu, blu, mag ]
 ]
 
-header = "\t".join(["row", "col","r", "g", "b", "a"])
-print(header)
-for rowIx, row in enumerate(cells):
-    for colIx, cell in enumerate(row):
-        print(rowIx, colIx, *cell, sep="\t")
+def print_pixel_data(pythonList):
+    titles = ["row", "col","r", "g", "b", "a"]
+    header = "\t".join(titles)
+    print(header)
+    for rowIx, row in enumerate(pythonList):
+        for colIx, pixel in enumerate(row):
+            print(rowIx, colIx, *pixel, sep="\t")
+
+size = 2
+factor = 2**size
+
+# Init matrix of None
+nbRows = factor * len(cells)
+nbCols = factor * len(cells[0])
+maxRow = nbRows - 1
+maxCol = nbCols - 1
+pythonList = [
+    [None for c in range(nbCols)] for r in range(nbRows)
+]
+
+def fill_cell(pythonList, cellRow, cellCol, cellValue, factor):
+    originRow = cellRow * factor
+    originCol = cellCol * factor
+    lastRow = originRow + factor
+    lastCol = originCol + factor
+    for pixelRow in range(originRow, lastRow):
+        for pixelCol in range(originCol, lastCol):
+            pythonList[pixelRow][pixelCol] = cellValue
+
+for cellRow, row in enumerate(cells):
+    for cellCol, cell in enumerate(row):
+            fill_cell(pythonList, cellRow, cellCol, cell, factor)
+
+# I want the top right colour to always be a single pixel.
+fill_cell(pythonList, 0, -1, cells[0][-2], factor)
+pythonList[0][-1] = cells[0][-1]
+
+print_pixel_data(pythonList)
+
