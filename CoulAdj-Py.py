2b49b46c60d78f9b10b6dc740d6f0e81d6279a8f
Amélia SZK <amelia.szk@protonmail.com>
2021-02-03 17:46:41 -0500

    Finalized CLI

--- CoulAdj-Py.py
+++ CoulAdj-Py.py
@@ -1,35 +1,84 @@
+# Computes, for each colour in the image, the list of all adjacent colours.
+# Copyright (C) 2021  Amélia SZK <amelia.szk@protonmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import imageio
 import numpy as np
 import argparse
 import time
-import sys
+import logging
 
-diags_help = "If present, will only consider the 4 edge neighbours to be adjacent"
-img_help = "The image file to process"
-res_help = "The TSV file where to store the results"
+version = "0.1"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dont-relate-diagonals", help=diags_help,
+# ~~~~~ Prologue ~~~~~
+
+# ~~~ Profiling ~~~
+startTime = time.perf_counter()
+
+# ~~~ Command Line Interface ~~~
+diags_flags = ("-d", "--dont-relate-diagonals")
+verbose_flags = ("-v", "--verbose")
+profile_flags = ("-p", "--profile")
+version_flags = ("-n", "--version")
+
+description_program = "Computes, for each colour in the image," \
+    " the list of all adjacent colours and writes the results to a TSV file."
+epilog_program = "CoulAdj-Py  Copyright (C) 2021  Amélia SZK." \
+    "\nReleased under GPL-3.0 License."
+
+help_diags = "if present, will only consider the 4 neighbours with a common edge" \
+    " (top, bottom, left, right) to be adjacent." \
+    " By default, all 8 neighbours are considered adjacent"
+help_verbose = "displays information about the file and the computations"
+help_profile = "(for developers; users should use {} instead)" \
+    " Prints the execution time to stderr regardless of logging level".format(verbose_flags[0])
+help_img = "The image file to process."
+help_res = "The TSV file in which to write the results. " \
+    "If it already exists, it will be erased and overwritten."
+
+parser = argparse.ArgumentParser(description=description_program, epilog=epilog_program)
+parser.add_argument(*diags_flags, help=help_diags,
     action="store_true", dest="dontRelateDiagonals")
-parser.add_argument("image", help=img_help, 
+parser.add_argument(*verbose_flags, help=help_verbose,
+    action="store_true")
+parser.add_argument(*version_flags, 
+    action="version", version="%(prog)s " + version)
+parser.add_argument(*profile_flags, help=help_profile,
+    action="store_true")
+parser.add_argument("image", help=help_img, 
     type=argparse.FileType("rb"))
-parser.add_argument("results", help=res_help, 
+parser.add_argument("results", help=help_res, 
     type=argparse.FileType("w"))
 args = parser.parse_args()
 
+exit()
+# ~~~ Logging ~~~
 
 # # Input
 
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
 source = args.image
 destination = args.results
-relateDiagonals = not args.dontRelateDiagonals # Default is True
+relateDiagonals = not args.dontRelateDiagonals
 print(source)
 print(destination)
 
 
 # # Processing
 
-startTime = time.perf_counter()
 
 image = imageio.imread(source)
 print(image.shape)
@@ -44,8 +93,6 @@ print(nbRows, nbColumns, maxRow, maxColumn)
 
 
 def process_pixel(pixelRow, pixelColumn):
-    if pixelColumn == 0 and pixelRow % 10 == 0:
-        print("Now starting pixel at row {} and column {}".format(pixelRow, pixelColumn))
     pixelColour = tuple(image[pixelRow, pixelColumn].tolist())
     process_neighbour(pixelColour, pixelRow, pixelColumn, 1, -1)
     process_neighbour(pixelColour, pixelRow, pixelColumn, 1, 1)
@@ -147,5 +194,5 @@ destination.write(stringyfied)
 # Report execution duration
 endTime = time.perf_counter()
 executionDuration = round(endTime - startTime, 3)
-print("{}".format(executionDuration), file=sys.stderr)
+print("{}".format(executionDuration))
 
