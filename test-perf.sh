bbdd10e074ccefbd75e1948370057533ff0d73c2
Amélia SZK <amelia.szk@protonmail.com>
2021-02-13 11:31:09 -0500

    Benchmarked Numpy all sizes

--- test-perf.sh
+++ test-perf.sh
@@ -13,28 +13,28 @@ GOLDEN_DIR='./tests/'
 # (Make sure the DIR variables end with a /backslash/ )
 
 # Usually, NB_LOOPS=10
-NB_LOOPS=10
+NB_LOOPS=1
 
 GOLDEN="${GOLDEN_DIR}golden.tsv"
 evaluate_size(){
     local SAMPLE="${SAMPLES_DIR}sample-size-$1.png"
     local RESULT="${RESULTS_DIR}result-size-$1.tsv"
     for ((i = 1 ; i <= $NB_LOOPS ; i++)); do
-        #echo -n "Size $1: "
+        echo -n "Size $1: "
         $COMMAND "$SAMPLE" "$RESULT"
     done
     cmp --silent "$GOLDEN" "$RESULT" || echo "Size $1 failed"
 }
 
-#evaluate_size 1
-#evaluate_size 2
-#evaluate_size 4
-#evaluate_size 8
+evaluate_size 1
+evaluate_size 2
+evaluate_size 4
+evaluate_size 8
 evaluate_size 16
-#evaluate_size 32
-#evaluate_size 64
-#evaluate_size 128
-#evaluate_size 256
-#evaluate_size 512
+evaluate_size 32
+evaluate_size 64
+evaluate_size 128
+evaluate_size 256
+evaluate_size 512
 #evaluate_size 1024
 echo "Performance test finished"
